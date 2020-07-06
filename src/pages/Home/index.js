0ab434365c1f8e4ea23b90987e649048ded3e0c1
DanielSoaresRocha <legoliga123@gmail.com>
2020-07-06 02:59:36 -0300

    adicionado sessão minha história

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './styles.css'
 
+import { BsDownload } from "react-icons/bs";
+
 import fundo from '../../assets/images/fundo.jpg'
-//import logo from '../../assets/images/logo2.jpg'
+import myImage from '../../assets/images/eu.jpg'
 
 function Home () {
     return (
@@ -23,7 +25,23 @@ function Home () {
                 <h1>DANIEL ROCHA</h1>
                 <hr />
                 <p>Desenvolvedor Full Stack Junior</p>
-                <button>Começar</button>
+                <a href='#history'>Começar</a>
+            </div>
+
+            <div className='history'>
+                <div className='text'>
+                    <h1 id='history'>Minha história</h1>
+                    <p>
+                        É simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                    </p>
+
+                    <div className='donwloadCV'>
+                        <button>Meu currículo</button>
+                        <BsDownload color={"white"} size={17} />
+                    </div>
+                </div>
+                <img src={myImage} alt='Minha imagem' />
+
             </div>
         </div>
     );
