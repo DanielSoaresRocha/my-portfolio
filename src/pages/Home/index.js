10121ef510beab97318d1d8a1e6c97492277d1ff
DanielSoaresRocha <legoliga123@gmail.com>
2020-07-06 01:40:11 -0300

    Apresentação finalizada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import './styles.css'
 
 import fundo from '../../assets/images/fundo.jpg'
-import logo from '../../assets/images/logo.jpg'
+//import logo from '../../assets/images/logo2.jpg'
 
 function Home () {
     return (
         <div className='containerHome'>
             <img src={fundo} alt='background' />
             <header>
-                <img src={logo} alt='logo' />
-
+                <div className='logo'>
+                    <h1>Daniel Rocha</h1>
+                </div>
+                <div className='menu'>
+                    <p>História</p>
+                    <p>Habilidades</p>
+                    <p>Projetos</p>
+                </div>
             </header>
+
+            <div className='presentation'>
+                <h1>DANIEL ROCHA</h1>
+                <hr />
+                <p>Desenvolvedor Full Stack Junior</p>
+                <button>Começar</button>
+            </div>
         </div>
     );
 }
