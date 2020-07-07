738b759e5365c845e6bb15a18880a4e5f12666ab
DanielSoaresRocha <legoliga123@gmail.com>
2020-07-07 04:03:37 -0300

    adicionado efeito de imagem estática

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -34,17 +34,30 @@ function Home () {
                 <div className='text'>
                     <h1 id='history'>Minha história</h1>
                     <p>
-                        É simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                        Comecei com programação aos 17 anos em 2017 durante o curso técnico de Automação Industrial.
+                        Me interessei pela área de programação e em 2018 entrei no curso Análise e Desenvolvimento de Sistemas
+                        e conclui o curso de Automação em 2019. Durante este período aumentei minha experiencia em mais linguagens
+                        de programação, entre elas C, Java, Kotlin, python e JavaScript.
+                        <br />
+                        Tenho uma maior com experiência com aplicações android + java, onde atualmente desenvolvo meu TCC
+                        utilizando essas tecnologias além de também envolver eletrônica (com microcontroladores arduino e esp32)
+                        e a plataforma Firebase para armazenamento em nuvem.
+                        <br />
+                            Atualmente estou me especializando em web front end, cada vez mais apaixonado pela linguagem javaScript,
+                            aprendendo e fazendo uso de seus Frameworks react.js, vue.js, e quasar. Estou a procura de um estágio nessas
+                            áreas afins para ganhar experiência com o mercado de trabalho.
                     </p>
-
-
                     <a href={curriculo} download="DanielSoares.pdf">
                         Meu currículo
                     <BsDownload className='spacing' />
                     </a>
                 </div>
-                <img src={myImage} alt='Minha imagem' />
 
+                <img src={myImage} alt='Minha imagem' />
+            </div>
+
+            <div className='skills'>
+                <h1>Habilidades</h1>
             </div>
         </div>
     );
